7e74f83d144a89af051062bb723c94070a017b72
achutanandaaltia-gif <achutanandaaltia@gmail.com>
2026-01-19 17:11:11 +0530

    lab 3 cp2

---
+++ length of string cp 2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char s[20];
+	int i, count=0;
+	printf("enter strings\n");
+	gets(s);
+	for (i =0 ;s[i]!='\0';i++)
+	{
+		count++;
+	}
+	printf("%d = length",i);
+	
+		
+		
+}
